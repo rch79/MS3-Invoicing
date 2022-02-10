c976722abe0e74b9923e55b632a1883593d4c6e1
rch79 <rhorta@gmail.com>
2022-02-10 16:04:06 +0000

    Add local host to ALOWED_HOSTS

--- django_appointment_billing_project/settings.py
+++ django_appointment_billing_project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-appointment-invoicing.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['django-appointment-invoicing.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
