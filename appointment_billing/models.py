3d3ba07645630862df72be8d275cbbbd9d3f40ee
rch79 <rhorta@gmail.com>
2022-02-10 14:51:36 +0000

    Create database models

--- appointment_billing/models.py
+++ appointment_billing/models.py
@@ -1,3 +1,77 @@
 from django.db import models
+# from cloudinary.models import CloudinaryField
 
 # Create your models here.
+APPOINTMENT_TYPES = (
+    (1, "Appointment"),
+    (2, "Appointment - Late Cancelation"),
+    (3, "No Show")
+)
+
+class Payee(models.Model):
+    '''Invoive recipients'''
+    payee_name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class PayeeAddress(models.Model):
+    '''Invoice recipient addresses'''
+    payee_name = models.ForeignKey(Payee, on_delete=models.CASCADE)
+    payee_address_line_1 = models.CharField(max_length=50)
+    payee_address_line_2 = models.CharField(max_length=50)
+    payee_address_city = models.CharField(max_lenght=20)
+    payee_address_county = models.CharField(max_length=25)
+    payee_address_eircode = models.CharField(max_length=7)
+
+    class Meta:
+        ordering = ['-payee_name']
+
+    def __str__(self):
+        return self.title
+
+
+class PaymentDetails(models.Model):
+    '''Remittance Details'''
+    payment_number = models.PositiveSmallIntegerField(unique=True)
+    payment_date = models.DateField()
+    payment_amount = models.DecimalField(decimal_places=2)
+    payment_method = models.CharField(max_length=15)
+    payment_is_cleared = models.BooleanField()
+
+    class Meta:
+        ordering = ['-payment_date']
+ 
+    def __str__(self):
+        return self.title
+
+
+class Invoice(models.Model):
+    '''Invoices'''
+    invoice_number = models.PositiveSmallIntegerField(unique=True)
+    payee_name = models.ForeignKey(Payee, on_delete=models.CASCADE)
+    invoice_date = models.DateField()
+    invoice_sent_date = models.DateField()
+    payment_number = models.ForeignKey(PaymentDetails, on_delete=models.SET_NULL)
+    payment_status = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-invoice_number']
+
+    def __str__(self):
+        return self.title
+
+
+class Appointment(models.Model):
+    '''Appointment Details'''
+    invoice_number = models.ForeignKey(Invoice, on_delete=models.SET_NULL)
+    appointment_datetime = models.DateTimeField()
+    appointment_type = models.CharField(choices=APPOINTMENT_TYPES, default=1)
+    appointment_fee = models.DecimalField(decimal_places=2)
+
+    class Meta:
+        ordering = ['-appointment_datetime']
+
+    def __str__(self):
+        return self.title
