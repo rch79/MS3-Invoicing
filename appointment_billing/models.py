685e42fd21613f62a63e669f69689c0fd85afff8
rch79 <rhorta@gmail.com>
2022-02-10 16:00:37 +0000

    Fix syntax on database models

--- appointment_billing/models.py
+++ appointment_billing/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 APPOINTMENT_TYPES = (
     (1, "Appointment"),
-    (2, "Appointment - Late Cancelation"),
-    (3, "No Show")
+    (2, "Late Cancellation"),
+    (3, "No-Show")
 )
 
 class Payee(models.Model):
@@ -21,7 +21,7 @@ class PayeeAddress(models.Model):
     payee_name = models.ForeignKey(Payee, on_delete=models.CASCADE)
     payee_address_line_1 = models.CharField(max_length=50)
     payee_address_line_2 = models.CharField(max_length=50)
-    payee_address_city = models.CharField(max_lenght=20)
+    payee_address_city = models.CharField(max_length=20)
     payee_address_county = models.CharField(max_length=25)
     payee_address_eircode = models.CharField(max_length=7)
 
@@ -36,13 +36,13 @@ class PaymentDetails(models.Model):
     '''Remittance Details'''
     payment_number = models.PositiveSmallIntegerField(unique=True)
     payment_date = models.DateField()
-    payment_amount = models.DecimalField(decimal_places=2)
+    payment_amount = models.DecimalField(decimal_places=2, max_digits=6)
     payment_method = models.CharField(max_length=15)
     payment_is_cleared = models.BooleanField()
 
     class Meta:
         ordering = ['-payment_date']
- 
+
     def __str__(self):
         return self.title
 
@@ -53,7 +53,9 @@ class Invoice(models.Model):
     payee_name = models.ForeignKey(Payee, on_delete=models.CASCADE)
     invoice_date = models.DateField()
     invoice_sent_date = models.DateField()
-    payment_number = models.ForeignKey(PaymentDetails, on_delete=models.SET_NULL)
+    payment_number = models.ForeignKey(
+        PaymentDetails, blank=True, default='', on_delete=models.SET_DEFAULT
+    )
     payment_status = models.BooleanField(default=False)
 
     class Meta:
@@ -65,10 +67,16 @@ class Invoice(models.Model):
 
 class Appointment(models.Model):
     '''Appointment Details'''
-    invoice_number = models.ForeignKey(Invoice, on_delete=models.SET_NULL)
+    invoice_number = models.ForeignKey(
+        Invoice, blank=True, default='', on_delete=models.SET_DEFAULT
+    )
     appointment_datetime = models.DateTimeField()
-    appointment_type = models.CharField(choices=APPOINTMENT_TYPES, default=1)
-    appointment_fee = models.DecimalField(decimal_places=2)
+    appointment_type = models.CharField(
+        choices=APPOINTMENT_TYPES, default=1, max_length=30, blank=False
+    )
+    appointment_fee = models.DecimalField(
+        decimal_places=2, max_digits=3, blank=False
+    )
 
     class Meta:
         ordering = ['-appointment_datetime']
